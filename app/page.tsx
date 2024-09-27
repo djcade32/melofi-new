06ab676ac9d2d9d8d2c5f5d83576f877f17044d6
Norman Cade <norman_cade32@yahoo.com>
2024-09-27 04:51:49 -0400

    testing

--- app/page.tsx
+++ app/page.tsx
@@ -14,9 +14,6 @@ export default function Home() {
           priority
         />
         <ol>
-          <li>
-            Get started by editing <code>app/page.tsx</code>.
-          </li>
           <li>Save and see your changes instantly.</li>
         </ol>
 
