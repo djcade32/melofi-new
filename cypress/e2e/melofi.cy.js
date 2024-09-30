1920b3b78d790019a3e850826f250fa17aae5556
Norman Cade <norman_cade32@yahoo.com>
2024-09-30 15:28:21 -0400

    Added baseUrl to cypress config file

--- cypress/e2e/melofi.cy.js
+++ cypress/e2e/melofi.cy.js
@@ -1,5 +1,5 @@
 describe("Melofi App E2E Test", () => {
   it("should navigate to the homepage", () => {
-    cy.visit("http://localhost:3000"); // Visit your app's URL
+    cy.visit("/"); // Visit your app's URL
   });
 });
