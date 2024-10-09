ded0aefdc474165cde7701555873c1755374cb3d
Norman Cade <norman_cade32@yahoo.com>
2024-10-08 21:28:37 -0400

    Cleaned up imports

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 import ActionBar from "./actionBar/ActionBar";
 import styles from "./header.module.css";
-import logo from "@/public/assets/logos/melofi-logo.png";
-import Image from "next/image";
 import RotatingLogo from "../rotatingLogo/RotatingLogo";
 
 const Header = () => {
