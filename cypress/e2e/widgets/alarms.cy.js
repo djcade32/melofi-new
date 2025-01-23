e0d0c31e32962d964a4c604399ff4f88f1aa2c21
Norman Cade <norman_cade32@yahoo.com>
2025-01-23 04:42:11 -0500

    delete alarms

--- cypress/e2e/widgets/alarms.cy.js
+++ cypress/e2e/widgets/alarms.cy.js
@@ -75,8 +75,7 @@ describe("Testing Alarms Widget", () => {
     cy.wait(1000);
     pressToolsButton();
     pressToolbarButton("alarms");
-    // getElementWithClassName("alarmsItem__delete_button").click({ force: true });
-    getElementWithClassName("alarmsItem__container").children().first().click({ force: true });
+    getElementWithClassName("alarmsItem__delete_button").click({ force: true, multiple: true });
     getElementWithClassName("alarmsItem__container").should("have.length", 0);
 
     // getElementWithClassName("alarms__empty").should("exist").contains("No Alarms Set");
