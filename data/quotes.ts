f82ad55ab90286e029ba22a3eff80795217f0677
Norman Cade <norman_cade32@yahoo.com>
2025-02-07 18:57:14 -0500

    added more quotes

--- data/quotes.ts
+++ data/quotes.ts
@@ -36,4 +36,272 @@ export const Quotes: Quote[] = [
     text: "The only way to achieve the impossible is to believe it is possible",
     author: "Charles Kingsleigh",
   },
+  {
+    id: "8",
+    text: "Success is not final, failure is not fatal: It is the courage to continue that counts.",
+    author: "Winston Churchill",
+  },
+  {
+    id: "9",
+    text: "Believe you can and you're halfway there.",
+    author: "Theodore Roosevelt",
+  },
+  {
+    id: "10",
+    text: "Do what you can, with what you have, where you are.",
+    author: "Theodore Roosevelt",
+  },
+  {
+    id: "11",
+    text: "It always seems impossible until it's done.",
+    author: "Nelson Mandela",
+  },
+  {
+    id: "12",
+    text: "Act as if what you do makes a difference. It does.",
+    author: "William James",
+  },
+  {
+    id: "13",
+    text: "Don't watch the clock; do what it does. Keep going.",
+    author: "Sam Levenson",
+  },
+  {
+    id: "14",
+    text: "You miss 100% of the shots you don’t take.",
+    author: "Wayne Gretzky",
+  },
+  {
+    id: "15",
+    text: "Dream big and dare to fail.",
+    author: "Norman Vaughan",
+  },
+  {
+    id: "16",
+    text: "Opportunities don't happen. You create them.",
+    author: "Chris Grosser",
+  },
+  {
+    id: "17",
+    text: "Do one thing every day that scares you.",
+    author: "Eleanor Roosevelt",
+  },
+  {
+    id: "18",
+    text: "Start where you are. Use what you have. Do what you can.",
+    author: "Arthur Ashe",
+  },
+  {
+    id: "19",
+    text: "Hardships often prepare ordinary people for an extraordinary destiny.",
+    author: "C.S. Lewis",
+  },
+  {
+    id: "20",
+    text: "Everything you’ve ever wanted is on the other side of fear.",
+    author: "George Addair",
+  },
+  {
+    id: "21",
+    text: "Happiness depends upon ourselves.",
+    author: "Aristotle",
+  },
+  {
+    id: "22",
+    text: "Do or do not. There is no try.",
+    author: "Yoda",
+  },
+  {
+    id: "23",
+    text: "With the new day comes new strength and new thoughts.",
+    author: "Eleanor Roosevelt",
+  },
+  {
+    id: "24",
+    text: "Failure is the condiment that gives success its flavor.",
+    author: "Truman Capote",
+  },
+  {
+    id: "25",
+    text: "Courage is resistance to fear, mastery of fear—not absence of fear.",
+    author: "Mark Twain",
+  },
+  {
+    id: "26",
+    text: "Keep your face always toward the sunshine—and shadows will fall behind you.",
+    author: "Walt Whitman",
+  },
+  {
+    id: "27",
+    text: "What lies behind us and what lies before us are tiny matters compared to what lies within us.",
+    author: "Ralph Waldo Emerson",
+  },
+  {
+    id: "28",
+    text: "It does not matter how slowly you go as long as you do not stop.",
+    author: "Confucius",
+  },
+  {
+    id: "29",
+    text: "The best way to predict the future is to create it.",
+    author: "Peter Drucker",
+  },
+  {
+    id: "30",
+    text: "Do what you feel in your heart to be right—for you’ll be criticized anyway.",
+    author: "Eleanor Roosevelt",
+  },
+  {
+    id: "31",
+    text: "Life is 10% what happens to us and 90% how we react to it.",
+    author: "Charles R. Swindoll",
+  },
+  {
+    id: "32",
+    text: "You cannot swim for new horizons until you have courage to lose sight of the shore.",
+    author: "William Faulkner",
+  },
+  {
+    id: "33",
+    text: "He who has a why to live can bear almost any how.",
+    author: "Friedrich Nietzsche",
+  },
+  {
+    id: "34",
+    text: "If opportunity doesn’t knock, build a door.",
+    author: "Milton Berle",
+  },
+  {
+    id: "35",
+    text: "Do what is right, not what is easy nor what is popular.",
+    author: "Roy T. Bennett",
+  },
+  {
+    id: "36",
+    text: "Doubt kills more dreams than failure ever will.",
+    author: "Suzy Kassem",
+  },
+  {
+    id: "37",
+    text: "A year from now you may wish you had started today.",
+    author: "Karen Lamb",
+  },
+  {
+    id: "38",
+    text: "No one can make you feel inferior without your consent.",
+    author: "Eleanor Roosevelt",
+  },
+  {
+    id: "39",
+    text: "Perfection is not attainable, but if we chase perfection we can catch excellence.",
+    author: "Vince Lombardi",
+  },
+  {
+    id: "40",
+    text: "Fall seven times and stand up eight.",
+    author: "Japanese Proverb",
+  },
+  {
+    id: "41",
+    text: "We are what we repeatedly do. Excellence, then, is not an act, but a habit.",
+    author: "Aristotle",
+  },
+  {
+    id: "42",
+    text: "Be the change that you wish to see in the world.",
+    author: "Mahatma Gandhi",
+  },
+  {
+    id: "43",
+    text: "Life isn’t about waiting for the storm to pass, it’s about learning to dance in the rain.",
+    author: "Vivian Greene",
+  },
+  {
+    id: "44",
+    text: "Opportunities multiply as they are seized.",
+    author: "Sun Tzu",
+  },
+  {
+    id: "45",
+    text: "If you want to lift yourself up, lift up someone else.",
+    author: "Booker T. Washington",
+  },
+  {
+    id: "46",
+    text: "A ship in harbor is safe, but that is not what ships are built for.",
+    author: "John A. Shedd",
+  },
+  {
+    id: "47",
+    text: "Act without expectation.",
+    author: "Lao Tzu",
+  },
+  {
+    id: "48",
+    text: "Happiness is not something ready made. It comes from your own actions.",
+    author: "Dalai Lama",
+  },
+  {
+    id: "49",
+    text: "Strive not to be a success, but rather to be of value.",
+    author: "Albert Einstein",
+  },
+  {
+    id: "50",
+    text: "If you want something you’ve never had, you must be willing to do something you’ve never done.",
+    author: "Thomas Jefferson",
+  },
+  {
+    id: "51",
+    text: "The only limit to our realization of tomorrow will be our doubts of today.",
+    author: "Franklin D. Roosevelt",
+  },
+  {
+    id: "52",
+    text: "You must think small to accomplish big things.",
+  },
+  {
+    id: "53",
+    text: "Nothing can be done without faith and confidence.",
+  },
+  {
+    id: "54",
+    text: "The only way to predict the future is to create it.",
+    author: "Peter Drucker",
+  },
+  {
+    id: "55",
+    text: "Every day is an opportunity.",
+  },
+  {
+    id: "56",
+    text: "ou do difficult things.",
+  },
+  {
+    id: "57",
+    text: "You are capable of more than you know.",
+    author: "Glinda the Good Witch",
+  },
+  {
+    id: "58",
+    text: "You are braver than you believe, stronger than you seem, and smarter than you think.",
+    author: "A.A. Milne",
+  },
+  {
+    id: "59",
+    text: "You are never too old to set another goal or to dream a new dream.",
+    author: "C.S. Lewis",
+  },
+  {
+    id: "60",
+    text: "Success is the product of daily habits.",
+  },
+  {
+    id: "61",
+    text: "You are the author of your own story.",
+  },
+  {
+    id: "62",
+    text: "What you give energy towards will manifest in your life.",
+  },
 ];
