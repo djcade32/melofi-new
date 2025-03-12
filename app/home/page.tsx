eb0f0b9c60c56943475f8a5522e25f4a5fe68189
Norman Cade <norman_cade32@yahoo.com>
2025-03-12 18:46:35 -0400

    Pricing section done

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -2,6 +2,7 @@ import Navbar from "@/landing_page_src/navbar/Navbar";
 import About from "@/landing_page_src/sections/about/About";
 import Features from "@/landing_page_src/sections/features/Features";
 import Hero from "@/landing_page_src/sections/hero/Hero";
+import Pricing from "@/landing_page_src/sections/pricing/Pricing";
 
 export default function LandingPage() {
   return (
@@ -10,6 +11,7 @@ export default function LandingPage() {
       <Hero />
       <About />
       <Features />
+      <Pricing />
     </div>
   );
 }
