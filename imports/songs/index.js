1a3ffe3beaa442522101f485509b72ee6525aae6
Norman Cade <norman_cade32@yahoo.com>
2025-03-19 15:47:30 -0400

    Test

--- imports/songs/index.js
+++ imports/songs/index.js
@@ -1,111 +1,111 @@
 import theRidge_allemIverson from "../../public/assets/songs/the-ridge_allem-iversom.mp3";
-import illMeetYouAtTheStation_glimlip_louk from "../../public/assets/songs/ill-meet-you-at-the-station_glimlip_x_louk.mp3";
-import laube_mondoLoops_kanisan from "../../public/assets/songs/laube_mondo-loops_x_kanisan.mp3";
-import messageInADream_mondoLoops from "../../public/assets/songs/message-in-a-dream_mondo-loops.mp3";
-import morningMoon_kanisan_wishesAndDreams from "../../public/assets/songs/morning-moon_kanisan_x_wishes-and-dreams.mp3";
-import nocturnal_steezyPrime from "../../public/assets/songs/nocturnal_steezy-prime.mp3";
-import redbone_lav8 from "../../public/assets/songs/redbone_lav8.mp3";
-import renderYourHeart_bluntone from "../../public/assets/songs/render-your-heart_bluntone.mp3";
-import shouGaNai_raimu from "../../public/assets/songs/shou-ga-nai_raimu.mp3";
-import sinkingFatigue_raimu from "../../public/assets/songs/sinking-fatigue_raimu.mp3";
-import takeoff_streamError from "../../public/assets/songs/takeoff_stream-error.mp3";
-import fallenHorizon_gCoope_oddPanda_earlyGarden from "../../public/assets/songs/fallen-horizon_gCoope_x_odd-panda_x_early-garden.mp3";
-import overTheMoon_teamAstro from "../../public/assets/songs/over-the-moon_team-astro.mp3";
-import cityOnAHill_lOutlander from "../../public/assets/songs/city-on-a-hill_lOutlander.mp3";
-import overgrown_tenno from "../../public/assets/songs/overgrown_tenno.mp3";
-import finalMoments_nadavCohen_kokoro from "../../public/assets/songs/final-moments_nadav-cohen_x_kokoro.mp3";
-import theBadParty_wys from "../../public/assets/songs/the-bad-party_wys.mp3";
-import ancestral_casiio from "../../public/assets/songs/ancestral_casiio.mp3";
-import edda_kanisan_darioLessing_claudia from "../../public/assets/songs/edda_kanisan_x_dario-lessing_x_claudia.mp3";
-import willowHill_spencerHunt from "../../public/assets/songs/willow-hill_spencer-hunt.mp3";
-import wellBeAlright_softy_lucidGreen from "../../public/assets/songs/well-be-alright_softy_x_lucid-green.mp3";
-import dontYouKnow_sebastianKamae_Aylior from "../../public/assets/songs/dontYouKnow_sebastianKamae_x_Aylior.mp3";
-import daylight_teamAstro from "../../public/assets/songs/daylight_teamAstro.mp3";
-import starryNight_eugenioIzzi_mondoLoops from "../../public/assets/songs/starryNight_eugenioIzzi_x_mondoLoops.mp3";
-import votaliya_chauSara from "../../public/assets/songs/votaliya_chauSara.mp3";
-import towardsTheMountains_plantGuy from "../../public/assets/songs/towardsTheMountains_plantGuy.mp3";
-import betterThanYesterday_squeeda_cxlt from "../../public/assets/songs/betterThanYesterday_squeeda_cxlt.mp3";
-import lunarSight_ticofaces_loafyBuilding from "../../public/assets/songs/lunarSight_ticofaces_loafyBuilding.mp3";
-import goldenClouds_rudyRaw_mello from "../../public/assets/songs/goldenClouds_rudyRaw_mello.mp3";
-import moonBeams_lofty_jaackson_whilst from "../../public/assets/songs/moonBeams_lofty_jaackson_whilst.mp3";
-import aLittleHelp_hmSurf from "../../public/assets/songs/aLittleHelp_hmSurf.mp3";
-import firstSteps_phlocalyst_myrad_hilaria from "../../public/assets/songs/first-steps_phlocalyst_x_myrad_x_hilaria.mp3";
-import gentleRain_softy_comodo_otisUbaka from "../../public/assets/songs/gentle-rain_softy_x_comodo_x_otisUbaka.mp3";
-import gamble_worldtraveller_maxMerseny_viktorMinsky from "../../public/assets/songs/gamble_worldtraveller_x_maxMerseny_x_viktorMinsky.mp3";
-import sentimentalMood_worldtraveller_maxMerseny from "../../public/assets/songs/sentimental-mood_worldtraveller_x_maxMerseny.mp3";
-import oldFriends_twoScents from "../../public/assets/songs/old-friends_twoScents.mp3";
-import shadesOfMySoul_phlocalyst_myriad_hilaria from "../../public/assets/songs/shades-of-my-soul_phlocalyst_x_myriad_x_hilaria.mp3";
-import ippo_hoko_damien_fleau from "../../public/assets/songs/ippo_hoko_x_damienFleau.mp3";
-import snoozeButton_purrpleCat from "../../public/assets/songs/snoozeButton_purrpleCat.mp3";
-import snowman_wys from "../../public/assets/songs/snowman_wys.mp3";
-import aloneTime_fleece_sogni_guleum from "../../public/assets/songs/aloneTime_fleece_x_sogni_x_guleum.mp3";
-import beforeYouGo_jhove from "../../public/assets/songs/beforeYouGo_jhove.mp3";
-import abandoned_dlj from "../../public/assets/songs/abandoned_dlj.mp3";
-import mrCatchy_sebastianKamae_Aylior from "../../public/assets/songs/mrCatchy_sebastianKamae_x_Aylior.mp3";
-import you_cocabona from "../../public/assets/songs/you_cocabona.mp3";
-import gatesOfHeaven_bluntOne from "../../public/assets/songs/gatesOfHeaven_bluntOne.mp3";
-import weightless_slingDilly_sleepermane from "../../public/assets/songs/weightless_slingDilly_sleepermane.mp3";
-import diascia_casiio from "../../public/assets/songs/diascia_casiio.mp3";
-import newPaths_ayzic_lennyLoops from "../../public/assets/songs/newPaths_ayzic_lennyLoops.mp3";
-import flyingDoodles_chauSara from "../../public/assets/songs/flyingDoodles_chauSara.mp3";
-import sleepermane_raindance from "../../public/assets/songs/sleepermane_raindance.mp3";
-import lilac_gMills from "../../public/assets/songs/lilac_gMills.mp3";
-import peacefulSleep_noSpirit from "../../public/assets/songs/peacefulSleep_noSpirit.mp3";
-import shimmersOfADream_noSpirit_kainbeats from "../../public/assets/songs/shimmersOfADream_noSpirit_kainbeats.mp3";
+// import illMeetYouAtTheStation_glimlip_louk from "../../public/assets/songs/ill-meet-you-at-the-station_glimlip_x_louk.mp3";
+// import laube_mondoLoops_kanisan from "../../public/assets/songs/laube_mondo-loops_x_kanisan.mp3";
+// import messageInADream_mondoLoops from "../../public/assets/songs/message-in-a-dream_mondo-loops.mp3";
+// import morningMoon_kanisan_wishesAndDreams from "../../public/assets/songs/morning-moon_kanisan_x_wishes-and-dreams.mp3";
+// import nocturnal_steezyPrime from "../../public/assets/songs/nocturnal_steezy-prime.mp3";
+// import redbone_lav8 from "../../public/assets/songs/redbone_lav8.mp3";
+// import renderYourHeart_bluntone from "../../public/assets/songs/render-your-heart_bluntone.mp3";
+// import shouGaNai_raimu from "../../public/assets/songs/shou-ga-nai_raimu.mp3";
+// import sinkingFatigue_raimu from "../../public/assets/songs/sinking-fatigue_raimu.mp3";
+// import takeoff_streamError from "../../public/assets/songs/takeoff_stream-error.mp3";
+// import fallenHorizon_gCoope_oddPanda_earlyGarden from "../../public/assets/songs/fallen-horizon_gCoope_x_odd-panda_x_early-garden.mp3";
+// import overTheMoon_teamAstro from "../../public/assets/songs/over-the-moon_team-astro.mp3";
+// import cityOnAHill_lOutlander from "../../public/assets/songs/city-on-a-hill_lOutlander.mp3";
+// import overgrown_tenno from "../../public/assets/songs/overgrown_tenno.mp3";
+// import finalMoments_nadavCohen_kokoro from "../../public/assets/songs/final-moments_nadav-cohen_x_kokoro.mp3";
+// import theBadParty_wys from "../../public/assets/songs/the-bad-party_wys.mp3";
+// import ancestral_casiio from "../../public/assets/songs/ancestral_casiio.mp3";
+// import edda_kanisan_darioLessing_claudia from "../../public/assets/songs/edda_kanisan_x_dario-lessing_x_claudia.mp3";
+// import willowHill_spencerHunt from "../../public/assets/songs/willow-hill_spencer-hunt.mp3";
+// import wellBeAlright_softy_lucidGreen from "../../public/assets/songs/well-be-alright_softy_x_lucid-green.mp3";
+// import dontYouKnow_sebastianKamae_Aylior from "../../public/assets/songs/dontYouKnow_sebastianKamae_x_Aylior.mp3";
+// import daylight_teamAstro from "../../public/assets/songs/daylight_teamAstro.mp3";
+// import starryNight_eugenioIzzi_mondoLoops from "../../public/assets/songs/starryNight_eugenioIzzi_x_mondoLoops.mp3";
+// import votaliya_chauSara from "../../public/assets/songs/votaliya_chauSara.mp3";
+// import towardsTheMountains_plantGuy from "../../public/assets/songs/towardsTheMountains_plantGuy.mp3";
+// import betterThanYesterday_squeeda_cxlt from "../../public/assets/songs/betterThanYesterday_squeeda_cxlt.mp3";
+// import lunarSight_ticofaces_loafyBuilding from "../../public/assets/songs/lunarSight_ticofaces_loafyBuilding.mp3";
+// import goldenClouds_rudyRaw_mello from "../../public/assets/songs/goldenClouds_rudyRaw_mello.mp3";
+// import moonBeams_lofty_jaackson_whilst from "../../public/assets/songs/moonBeams_lofty_jaackson_whilst.mp3";
+// import aLittleHelp_hmSurf from "../../public/assets/songs/aLittleHelp_hmSurf.mp3";
+// import firstSteps_phlocalyst_myrad_hilaria from "../../public/assets/songs/first-steps_phlocalyst_x_myrad_x_hilaria.mp3";
+// import gentleRain_softy_comodo_otisUbaka from "../../public/assets/songs/gentle-rain_softy_x_comodo_x_otisUbaka.mp3";
+// import gamble_worldtraveller_maxMerseny_viktorMinsky from "../../public/assets/songs/gamble_worldtraveller_x_maxMerseny_x_viktorMinsky.mp3";
+// import sentimentalMood_worldtraveller_maxMerseny from "../../public/assets/songs/sentimental-mood_worldtraveller_x_maxMerseny.mp3";
+// import oldFriends_twoScents from "../../public/assets/songs/old-friends_twoScents.mp3";
+// import shadesOfMySoul_phlocalyst_myriad_hilaria from "../../public/assets/songs/shades-of-my-soul_phlocalyst_x_myriad_x_hilaria.mp3";
+// import ippo_hoko_damien_fleau from "../../public/assets/songs/ippo_hoko_x_damienFleau.mp3";
+// import snoozeButton_purrpleCat from "../../public/assets/songs/snoozeButton_purrpleCat.mp3";
+// import snowman_wys from "../../public/assets/songs/snowman_wys.mp3";
+// import aloneTime_fleece_sogni_guleum from "../../public/assets/songs/aloneTime_fleece_x_sogni_x_guleum.mp3";
+// import beforeYouGo_jhove from "../../public/assets/songs/beforeYouGo_jhove.mp3";
+// import abandoned_dlj from "../../public/assets/songs/abandoned_dlj.mp3";
+// import mrCatchy_sebastianKamae_Aylior from "../../public/assets/songs/mrCatchy_sebastianKamae_x_Aylior.mp3";
+// import you_cocabona from "../../public/assets/songs/you_cocabona.mp3";
+// import gatesOfHeaven_bluntOne from "../../public/assets/songs/gatesOfHeaven_bluntOne.mp3";
+// import weightless_slingDilly_sleepermane from "../../public/assets/songs/weightless_slingDilly_sleepermane.mp3";
+// import diascia_casiio from "../../public/assets/songs/diascia_casiio.mp3";
+// import newPaths_ayzic_lennyLoops from "../../public/assets/songs/newPaths_ayzic_lennyLoops.mp3";
+// import flyingDoodles_chauSara from "../../public/assets/songs/flyingDoodles_chauSara.mp3";
+// import sleepermane_raindance from "../../public/assets/songs/sleepermane_raindance.mp3";
+// import lilac_gMills from "../../public/assets/songs/lilac_gMills.mp3";
+// import peacefulSleep_noSpirit from "../../public/assets/songs/peacefulSleep_noSpirit.mp3";
+// import shimmersOfADream_noSpirit_kainbeats from "../../public/assets/songs/shimmersOfADream_noSpirit_kainbeats.mp3";
 
 export {
   theRidge_allemIverson,
-  illMeetYouAtTheStation_glimlip_louk,
-  laube_mondoLoops_kanisan,
-  messageInADream_mondoLoops,
-  morningMoon_kanisan_wishesAndDreams,
-  nocturnal_steezyPrime,
-  redbone_lav8,
-  renderYourHeart_bluntone,
-  shouGaNai_raimu,
-  sinkingFatigue_raimu,
-  takeoff_streamError,
-  fallenHorizon_gCoope_oddPanda_earlyGarden,
-  overTheMoon_teamAstro,
-  cityOnAHill_lOutlander,
-  overgrown_tenno,
-  finalMoments_nadavCohen_kokoro,
-  theBadParty_wys,
-  ancestral_casiio,
-  edda_kanisan_darioLessing_claudia,
-  willowHill_spencerHunt,
-  wellBeAlright_softy_lucidGreen,
-  dontYouKnow_sebastianKamae_Aylior,
-  daylight_teamAstro,
-  starryNight_eugenioIzzi_mondoLoops,
-  votaliya_chauSara,
-  towardsTheMountains_plantGuy,
-  betterThanYesterday_squeeda_cxlt,
-  lunarSight_ticofaces_loafyBuilding,
-  goldenClouds_rudyRaw_mello,
-  moonBeams_lofty_jaackson_whilst,
-  aLittleHelp_hmSurf,
-  firstSteps_phlocalyst_myrad_hilaria,
-  gentleRain_softy_comodo_otisUbaka,
-  gamble_worldtraveller_maxMerseny_viktorMinsky,
-  sentimentalMood_worldtraveller_maxMerseny,
-  oldFriends_twoScents,
-  shadesOfMySoul_phlocalyst_myriad_hilaria,
-  ippo_hoko_damien_fleau,
-  snoozeButton_purrpleCat,
-  snowman_wys,
-  aloneTime_fleece_sogni_guleum,
-  beforeYouGo_jhove,
-  abandoned_dlj,
-  mrCatchy_sebastianKamae_Aylior,
-  you_cocabona,
-  gatesOfHeaven_bluntOne,
-  weightless_slingDilly_sleepermane,
-  diascia_casiio,
-  newPaths_ayzic_lennyLoops,
-  flyingDoodles_chauSara,
-  sleepermane_raindance,
-  lilac_gMills,
-  peacefulSleep_noSpirit,
-  shimmersOfADream_noSpirit_kainbeats,
+  // illMeetYouAtTheStation_glimlip_louk,
+  // laube_mondoLoops_kanisan,
+  // messageInADream_mondoLoops,
+  // morningMoon_kanisan_wishesAndDreams,
+  // nocturnal_steezyPrime,
+  // redbone_lav8,
+  // renderYourHeart_bluntone,
+  // shouGaNai_raimu,
+  // sinkingFatigue_raimu,
+  // takeoff_streamError,
+  // fallenHorizon_gCoope_oddPanda_earlyGarden,
+  // overTheMoon_teamAstro,
+  // cityOnAHill_lOutlander,
+  // overgrown_tenno,
+  // finalMoments_nadavCohen_kokoro,
+  // theBadParty_wys,
+  // ancestral_casiio,
+  // edda_kanisan_darioLessing_claudia,
+  // willowHill_spencerHunt,
+  // wellBeAlright_softy_lucidGreen,
+  // dontYouKnow_sebastianKamae_Aylior,
+  // daylight_teamAstro,
+  // starryNight_eugenioIzzi_mondoLoops,
+  // votaliya_chauSara,
+  // towardsTheMountains_plantGuy,
+  // betterThanYesterday_squeeda_cxlt,
+  // lunarSight_ticofaces_loafyBuilding,
+  // goldenClouds_rudyRaw_mello,
+  // moonBeams_lofty_jaackson_whilst,
+  // aLittleHelp_hmSurf,
+  // firstSteps_phlocalyst_myrad_hilaria,
+  // gentleRain_softy_comodo_otisUbaka,
+  // gamble_worldtraveller_maxMerseny_viktorMinsky,
+  // sentimentalMood_worldtraveller_maxMerseny,
+  // oldFriends_twoScents,
+  // shadesOfMySoul_phlocalyst_myriad_hilaria,
+  // ippo_hoko_damien_fleau,
+  // snoozeButton_purrpleCat,
+  // snowman_wys,
+  // aloneTime_fleece_sogni_guleum,
+  // beforeYouGo_jhove,
+  // abandoned_dlj,
+  // mrCatchy_sebastianKamae_Aylior,
+  // you_cocabona,
+  // gatesOfHeaven_bluntOne,
+  // weightless_slingDilly_sleepermane,
+  // diascia_casiio,
+  // newPaths_ayzic_lennyLoops,
+  // flyingDoodles_chauSara,
+  // sleepermane_raindance,
+  // lilac_gMills,
+  // peacefulSleep_noSpirit,
+  // shimmersOfADream_noSpirit_kainbeats,
 };
