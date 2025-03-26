21b338e138871a4cde9badcec07efd38d891027c
Norman Cade <norman_cade32@yahoo.com>
2025-03-26 14:57:14 -0400

    Set page titles and open graph image

--- app/legal/terms-and-conditions/page.tsx
+++ app/legal/terms-and-conditions/page.tsx
@@ -1,4 +1,9 @@
+import { Metadata } from "next";
 import "../legal-global.css";
+
+export const metadata: Metadata = {
+  title: "Terms and Conditions",
+};
 
 export default function TermsAndConditions() {
   return (
