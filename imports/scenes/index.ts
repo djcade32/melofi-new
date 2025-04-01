910a6a44a96884af2a7b8fd8d021c4af162c0d93
Norman Cade <norman_cade32@yahoo.com>
2025-04-01 11:00:10 -0400

    Move back into public

--- imports/scenes/index.ts
+++ imports/scenes/index.ts
@@ -1,19 +1,19 @@
-import girlInCafeVid from "@/data/videos/girl-in-cafe_vid.mp4";
-import neighborhoodCafeVid from "@/data/videos/neighborhood-cafe_vid.mp4";
-import cozyBedroomVid from "@/data/videos/cozy-bedroom_vid.mp4";
-import cityStudioVid from "@/data/videos/city-studio_vid.mp4";
-import musicRoomVid from "@/data/videos/music-room_vid.mp4";
-import neonCityVid from "@/data/videos/neon-city_vid.mp4";
-import serenityCabinVid from "@/data/videos/serenity-cabin_vid.mp4";
-import girlAtDuskVid from "@/data/videos/girl-at-dusk_vid.mp4";
-import cozyRainNightVid from "@/data/videos/cozy-rain-night_vid.mp4";
-import neonNightMarketVid from "@/data/videos/neon-night-market_vid.mp4";
-import modernLivingRoomVid from "@/data/videos/modern-living-room_vid.mp4";
-import theOfficeVid from "@/data/videos/the-office_vid.mp4";
-import pastelParadiseVid from "@/data/videos/pastel-paradise_vid.mp4";
-import treeHouseVid from "@/data/videos/tree-house_vid.mp4";
-import winterCafeVid from "@/data/videos/winter-cafe_vid.mp4";
-import bookLoverVid from "@/data/videos/book-lover_vid.mp4";
+import girlInCafeVid from "@/public/assets/videos/girl-in-cafe_vid.mp4";
+import neighborhoodCafeVid from "@/public/assets/videos/neighborhood-cafe_vid.mp4";
+import cozyBedroomVid from "@/public/assets/videos/cozy-bedroom_vid.mp4";
+import cityStudioVid from "@/public/assets/videos/city-studio_vid.mp4";
+import musicRoomVid from "@/public/assets/videos/music-room_vid.mp4";
+import neonCityVid from "@/public/assets/videos/neon-city_vid.mp4";
+import serenityCabinVid from "@/public/assets/videos/serenity-cabin_vid.mp4";
+import girlAtDuskVid from "@/public/assets/videos/girl-at-dusk_vid.mp4";
+import cozyRainNightVid from "@/public/assets/videos/cozy-rain-night_vid.mp4";
+import neonNightMarketVid from "@/public/assets/videos/neon-night-market_vid.mp4";
+import modernLivingRoomVid from "@/public/assets/videos/modern-living-room_vid.mp4";
+import theOfficeVid from "@/public/assets/videos/the-office_vid.mp4";
+import pastelParadiseVid from "@/public/assets/videos/pastel-paradise_vid.mp4";
+import treeHouseVid from "@/public/assets/videos/tree-house_vid.mp4";
+import winterCafeVid from "@/public/assets/videos/winter-cafe_vid.mp4";
+import bookLoverVid from "@/public/assets/videos/book-lover_vid.mp4";
 
 export {
   girlInCafeVid,
