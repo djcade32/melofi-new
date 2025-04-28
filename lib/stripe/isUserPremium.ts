23e3171086d9e205484d336cf013462c9ef500a7
Norman Cade <norman_cade32@yahoo.com>
2025-04-28 05:57:43 -0400

    Able to distinguish a lifetime user from a premium user

--- lib/stripe/isUserPremium.ts
+++ lib/stripe/isUserPremium.ts
@@ -1,10 +1,12 @@
 import { UserMembership } from "@/enums/general";
 import { getFirebaseAuth } from "../firebase/firebaseClient";
 import { onAuthStateChanged } from "firebase/auth";
+import { collection, getDocs, getFirestore, orderBy, query, where } from "firebase/firestore";
 
 const auth = getFirebaseAuth();
 
 export default function isUserPremium(): Promise<UserMembership> {
+  console.log("Checking user premium status...");
   return new Promise((resolve) => {
     if (process.env.NEXT_PUBLIC_IS_CYPRESS) {
       return resolve("premium");
@@ -20,7 +22,24 @@ export default function isUserPremium(): Promise<UserMembership> {
       try {
         await user.getIdToken(true);
         const decodedToken = await user.getIdTokenResult();
-        console.log("Decoded token:", decodedToken.claims);
+
+        // Check Firestore for the user's paymentType
+        const db = getFirestore();
+        const checkoutSessionsRef = `users/${user.uid}/checkout_sessions`;
+
+        // Query checkout sessions, order by creation date, and filter for one-time payment type
+        const sessionsQuery = query(
+          collection(db, checkoutSessionsRef),
+          orderBy("created", "desc"), // Assuming you have a createdAt field
+          where("paymentType", "==", "one-time") // Check for lifetime payment
+        );
+        const snapshot = await getDocs(sessionsQuery);
+
+        if (!snapshot.empty) {
+          // If a "one-time" paymentType exists, this user has lifetime access
+          return resolve("premium"); // Lifetime access detected
+        }
+
         resolve(decodedToken.claims?.stripeRole as UserMembership | "free");
       } catch (error) {
         console.error("Error getting token:", error);
